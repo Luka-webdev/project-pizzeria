fd18e05da62fefcad0e09c1d8ee67f7ee75cd9fd
Łukasz Pełka <lukasz.pelka.ziip@gmail.com>
2020-11-28 21:24:53 +0100

    Run accordion for products

--- src/js/script.js
+++ src/js/script.js
@@ -56,6 +56,7 @@
       const thisProduct = this;
       thisProduct.id = id;
       thisProduct.data = data;
+      thisProduct.initAccordion();
       thisProduct.renderInMenu();
       console.log('new Product:', thisProduct);
     }
@@ -70,8 +71,26 @@
       /* add element to menu */
       menuContainer.appendChild(thisProduct.element);
     }
-
+    initAccordion(){
+      const thisProduct = this;
+  
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = ???;
+  
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function(event) {
+        /* prevent default action for event */
+  
+        /* find active product (product that has active class) */
+  
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+  
+        /* toggle active class on thisProduct.element */
+      });
+  
+    }
   }
+  
   const app = {
     initMenu: function(){
       const thisApp = this;
